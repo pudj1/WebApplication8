c19c90aee168ea2a43e3ccc94291ca89179e9812
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-11-26 22:08:45 +0200

    Add project files.

---
+++ WebApplication8/Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using WebApplication8.Data;
+
+    public class CompanyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompanyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var companies = _context.Company.ToList();
+            return View(companies);
+        }
+    }
+}
---
+++ WebApplication8/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication8.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using WebApplication8.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Company { get; set; }
+    }
+}
